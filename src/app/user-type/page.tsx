b267590e9bd3a808d28a7761f71a62e2335354ea
gyrroa <gyrroacquiat007@gmail.com>
2025-04-29 23:10:22 +0800

    added assets for create-account pages

--- src/app/user-type/page.tsx
+++ src/app/user-type/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-sans)] bg-[#F2E7DC]">
             <main className="flex flex-col items-center text-center justify-center h-screen gap-[20px] md:gap-[41px] select-none">
-                <img src="create-account/user-type.svg" alt="user-type" className="w-[315px] h-auto" />
+                <img src="create-account/user-type.svg" alt="user-type" className="w-[315px] h-auto drop-shadow-[0px_8px_16px_rgba(108,60,45,0.35)]" />
                 <div className="flex flex-col">
                     <h1 className="font-extrabold text-[24px] md:text-4xl text-[#F90] [text-shadow:0_0_4px_rgba(255,153,0,0.35)] leading-tight">Get a <span className="text-[#C45500]">parent</span> to set up<br /> the app</h1>
                 </div>
